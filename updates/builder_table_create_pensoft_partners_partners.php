32a3b52e56a54bedd7f1bc8c5d9959154d82c49c
root <root@project-dev.pensoft.local>
2020-12-21 13:11:00 +0200

    update updates/builder_table_create_pensoft_partners_partners.php

--- updates/builder_table_create_pensoft_partners_partners.php
+++ updates/builder_table_create_pensoft_partners_partners.php
@@ -7,24 +7,28 @@ class BuilderTableCreatePensoftPartnersPartners extends Migration
 {
     public function up()
     {
-        Schema::create('pensoft_partners_partners', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            $table->text('content')->nullable();
-            $table->integer('country_id')->nullable();
-            $table->string('country_code', 2)->nullable();
-            $table->string('title')->nullable();
-            $table->integer('city_id')->nullable();
-            $table->string('email')->nullable();
-            $table->string('instituion')->nullable();
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-        });
+        if (!Schema::hasTable('pensoft_partners_partners')) {
+            Schema::create('pensoft_partners_partners', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->text('content')->nullable();
+                $table->integer('country_id')->nullable();
+                $table->string('country_code', 2)->nullable();
+                $table->string('title')->nullable();
+                $table->integer('city_id')->nullable();
+                $table->string('email')->nullable();
+                $table->string('instituion')->nullable();
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+            });
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('pensoft_partners_partners');
+        if (Schema::hasTable('pensoft_partners_partners')) {
+            Schema::dropIfExists('pensoft_partners_partners');
+        }
     }
 }
