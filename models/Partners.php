4c91508d4c161415fb1363ae4f65574fad02d938
root <root@project-dev.pensoft.local>
2023-10-23 12:55:20 +0300

    translate

--- models/Partners.php
+++ models/Partners.php
@@ -4,7 +4,8 @@ use Database\Tester\Models\Country;
 use Pensoft\Cardprofiles\Models\Profiles;
 use RainLab\Location\Models\Country as CountryModel;
 use Model;
-
+use BackendAuth;
+use Validator;
 /**
  * Model
  */
@@ -23,6 +24,18 @@ class Partners extends Model
      * @var array Validation rules
      */
     public $rules = [
+    ];
+
+    /**
+     * @var array Translatable fields
+     */
+    public $translatable = [
+        'content',
+        'title',
+        'instituion',
+        'type',
+        'add_description',
+
     ];
 
 	protected $jsonable = [
@@ -63,5 +76,45 @@ class Partners extends Model
 	{
 		return $this->instituion . " - " . $this->type;
 	}
+    // Add below function use for get current user details
+    public function diff(){
+        $history = $this->revision_history;
+    }
+    public function getRevisionableUser()
+    {
+        return BackendAuth::getUser()->id;
+    }
 
+            /**
+     * Add translation support to this model, if available.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        Validator::extend(
+            'json',
+            function ($attribute, $value, $parameters) {
+                json_decode($value);
+
+                return json_last_error() == JSON_ERROR_NONE;
+            }
+        );
+
+        // Call default functionality (required)
+        parent::boot();
+
+        // Check the translate plugin is installed
+        if (!class_exists('RainLab\Translate\Behaviors\TranslatableModel')) {
+            return;
+        }
+
+        // Extend the constructor of the model
+        self::extend(
+            function ($model) {
+                // Implement the translatable behavior
+                $model->implement[] = 'RainLab.Translate.Behaviors.TranslatableModel';
+            }
+        );
+    }
 }
